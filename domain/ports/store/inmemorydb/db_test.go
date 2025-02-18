f0b3d98c6092bba04fdf71c99f29f856a6d5d5e5
alexlucaci <alexlucaci@github.dev>
2025-02-18 13:58:55 +0200

    db: added delete and list ports; added tests

--- domain/ports/store/inmemorydb/db_test.go
+++ domain/ports/store/inmemorydb/db_test.go
@@ -142,3 +142,118 @@ func TestUpdatePort_EmptyUpdate(t *testing.T) {
 	require.NotNil(t, savedPort)
 	require.Equal(t, port, savedPort, "comparing saved port with original port")
 }
+
+func TestDeletePort(t *testing.T) {
+	ctx := context.Background()
+	store := NewInMemoryDB()
+	id := "AEAJM"
+	port := ports.Port{
+		Name:        "Ajman",
+		City:        "Ajman",
+		Country:     "United Arab Emirates",
+		Alias:       []string{},
+		Regions:     []string{},
+		Coordinates: []float64{55.5136433, 25.4052165},
+		Province:    "Ajman",
+		Timezone:    "Asia/Dubai",
+		Unlocs:      []string{"AEAJM"},
+		Code:        "52000",
+	}
+
+	err := store.Create(ctx, id, port)
+	require.NoError(t, err, "creating port")
+
+	got, err := store.Get(ctx, id)
+	require.NoError(t, err, "getting port after creation")
+	require.NotEmpty(t, got)
+
+	err = store.Delete(ctx, id)
+	require.NoError(t, err, "deleting port")
+
+	_, err = store.Get(ctx, id)
+	require.EqualError(t, err, ports.ErrNotFound.Error())
+}
+
+func TestListPorts_limit_less_than_length(t *testing.T) {
+	ctx := context.Background()
+	store := NewInMemoryDB()
+	id := "AEAJM"
+	port := ports.Port{
+		Name:        "Ajman",
+		City:        "Ajman",
+		Country:     "United Arab Emirates",
+		Alias:       []string{},
+		Regions:     []string{},
+		Coordinates: []float64{55.5136433, 25.4052165},
+		Province:    "Ajman",
+		Timezone:    "Asia/Dubai",
+		Unlocs:      []string{"AEAJM"},
+		Code:        "52000",
+	}
+
+	err := store.Create(ctx, id, port)
+	require.NoError(t, err, "creating 1st port")
+
+	err = store.Create(ctx, id+"_1", port)
+	require.NoError(t, err, "creating 2nd port")
+
+	listedPorts, err := store.List(ctx, 1)
+	require.NoError(t, err, "listing ports with limit 1")
+	require.Equal(t, 1, len(listedPorts), "checking length of listed ports")
+}
+
+func TestListPorts_limit_equal_with_length(t *testing.T) {
+	ctx := context.Background()
+	store := NewInMemoryDB()
+	id := "AEAJM"
+	port := ports.Port{
+		Name:        "Ajman",
+		City:        "Ajman",
+		Country:     "United Arab Emirates",
+		Alias:       []string{},
+		Regions:     []string{},
+		Coordinates: []float64{55.5136433, 25.4052165},
+		Province:    "Ajman",
+		Timezone:    "Asia/Dubai",
+		Unlocs:      []string{"AEAJM"},
+		Code:        "52000",
+	}
+
+	err := store.Create(ctx, id, port)
+	require.NoError(t, err, "creating 1st port")
+
+	err = store.Create(ctx, id+"_1", port)
+	require.NoError(t, err, "creating 2nd port")
+
+	listedPorts, err := store.List(ctx, 2)
+	require.NoError(t, err, "listing ports with limit 1")
+	require.Equal(t, 2, len(listedPorts), "checking length of listed ports")
+}
+
+func TestListPorts_limit_greater_than_length(t *testing.T) {
+	ctx := context.Background()
+	store := NewInMemoryDB()
+	id := "AEAJM"
+	port := ports.Port{
+		Name:        "Ajman",
+		City:        "Ajman",
+		Country:     "United Arab Emirates",
+		Alias:       []string{},
+		Regions:     []string{},
+		Coordinates: []float64{55.5136433, 25.4052165},
+		Province:    "Ajman",
+		Timezone:    "Asia/Dubai",
+		Unlocs:      []string{"AEAJM"},
+		Code:        "52000",
+	}
+
+	err := store.Create(ctx, id, port)
+	require.NoError(t, err, "creating 1st port")
+
+	err = store.Create(ctx, id+"_1", port)
+	require.NoError(t, err, "creating 2nd port")
+
+	listedPorts, err := store.List(ctx, 3)
+	require.NoError(t, err, "listing ports with limit 1")
+	require.Equal(t, 2, len(listedPorts), "checking length of listed ports")
+}
